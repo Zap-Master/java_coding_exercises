5569347bf976d4c2c910a8fac998550612c89f48
Zap-Master <kim.lidiya2011@gmail.com>
2020-11-29 15:27:16 +0000

    Implemented 1st exercise - capitalizeWord & generateInitials

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -5,12 +5,14 @@ import java.util.List;
 public class Exercise001 {
     public String capitalizeWord(String word) {
         // Add your code here
-        return "";
+        return word.substring(0,1).toUpperCase() + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
         // Add your code here
-        return "";
+        String result = "";
+        result = (firstName.substring(0,1)).toUpperCase() + '.' + (lastName.substring(0,1)).toUpperCase();
+        return result;
     }
 
     public double addVat(double originalPrice, double vatRate) {
