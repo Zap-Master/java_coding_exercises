a39ccc669e7f10be5c2a220b4daf935ae1ad2825
Zap-Master <kim.lidiya2011@gmail.com>
2020-12-25 14:37:18 +0000

    Implemented Exercise 001 function countLinuxUsers

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -2,28 +2,25 @@ package com.techreturners.exercise001;
 
 import java.util.List;
 
+
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
         return word.substring(0,1).toUpperCase() + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        String result = "";
-        result = (firstName.substring(0,1)).toUpperCase() + '.' + (lastName.substring(0,1)).toUpperCase();
-        return result;
+        String initials = "";
+        initials = (firstName.substring(0,1)).toUpperCase() + '.' + (lastName.substring(0,1)).toUpperCase();
+        return initials;
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
         double result = originalPrice + originalPrice*vatRate/100;
         String strValue = String.format("%.2f", result);
         return  Double.parseDouble(strValue);
     }
 
     public String reverse(String sentence) {
-        // Add your code here
         StringBuilder str1 = new StringBuilder();
         str1.append(sentence);
         str1 = str1.reverse();
@@ -31,7 +28,13 @@ public class Exercise001 {
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+        int counterLinuxUsers = 0;
+        for (User user : users) {
+            //System.out.println(user.getUsername());
+            if (user.getType() == "Linux") {
+                counterLinuxUsers ++;
+            }
+        }               
+        return counterLinuxUsers;
     }
 }
