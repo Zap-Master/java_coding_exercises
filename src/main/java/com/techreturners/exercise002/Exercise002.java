c486c5535fa815f4304b9b2bf61d97c431d866c0
Zap-Master <kim.lidiya2011@gmail.com>
2020-12-25 15:06:26 +0000

    Exercise 002 completed

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,14 +2,16 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
-        return false;
+    public boolean isFromManchester(Person person) {
+        if (person.getCity() == "Manchester") {
+            return true;
+        } else return false;
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
+    public boolean canWatchFilm(Person person, int ageLimit) {
+        if (person.getAge()< ageLimit) {
+            return false;
+        } else return true;
     }
     
 }
